31bf21a323fbb12e458e930a60f313228b2f05de
Windwalk <windwalk-bushido@protonmail.com>
2022-03-26 23:44:46 +0100

    Init

---
+++ components/head.js
@@ -0,0 +1,8 @@
+<Head>
+  <title>Muh Blog</title>
+  <link rel="icon" href="/favicon.ico" />
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Montserrat&display=swap" rel="stylesheet" />
+</Head>;
