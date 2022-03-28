7958d70dfb38bf59cd77a69d61c62ecc6de7ed50
Windwalk <windwalk-bushido@protonmail.com>
2022-03-28 20:06:19 +0200

    Wrote short, real bio.

--- pages/index.js
+++ pages/index.js
@@ -22,25 +22,55 @@ export default function Blog({ allPostsData }) {
       <Nav />
       <div className="flex justify-center items-center w-full pt-8 pb-16 bg-gray-900 text-white">
         <div className="flex justify-between items-start w-8/12">
-          <Image src="/me.jpg" alt="picture of Milijan Mosić." width="2500" height="4000" className="rounded-xl shadow-xl" />
-          <p className="ml-8">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p className="mr-4 ml-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p className="mr-8">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <Image src="/me.jpg" alt="picture of Milijan Mosić." width="1250" height="2000" className="rounded-xl shadow-xl" />
+          <div className="ml-8">
+            <p className="">
+              Hello there, web developer here. Minimalist. Linux/BSD advocate. Cat lover. Currently unemployed and the student
+              of vocational studies with major in e-business. I "fell in love" with computers 20 years ago and that's why the
+              occuring problem is that I want to know everything about them... Right now I'm involved with learning{" "}
+              <a
+                href="https://tryhackme.com/p/windwalk"
+                className="transition duration-150 ease-out underline text-green-300 hover:text-white"
+              >
+                cybersecurity
+              </a>
+              , writting Bash scripts for my{" "}
+              <a
+                href="https://archlinux.org/"
+                className="transition duration-150 ease-out underline text-green-300 hover:text-white"
+              >
+                Arch Linux
+              </a>{" "}
+              rice,{" "}
+              <a
+                href="https://github.com/windwalk-bushido"
+                className="transition duration-150 ease-out underline text-green-300 hover:text-white"
+              >
+                making websites
+              </a>{" "}
+              (Vue + Flask) and{" "}
+              <a
+                href="https://projecteuler.net/"
+                className="transition duration-150 ease-out underline text-green-300 hover:text-white"
+              >
+                solving math problems
+              </a>{" "}
+              with Go lang. Also, I would like to get involved with Raspberry Pi and microcontrollers in the future.
+            </p>
+            <p className="mt-8">
+              This blog is going to be about... everything. I would like to make single, online place for myself and to treat it
+              kinda like public diary. At the same time, while writting, I should treat it also like whole world is watching...
+              weird isn't it? Right now, man is divided between a lot of social media sites and that sucks. This website will
+              serve the purpose of keeping all personal (past and future) posts in one place. To get in contact with me use{" "}
+              <a
+                href="mailto:milijan-mosic@protonmail.com"
+                className="transition duration-150 ease-out underline text-green-300 hover:text-white"
+              >
+                milijan-mosic@protonmail.com
+              </a>{" "}
+              email.
+            </p>
+          </div>
         </div>
       </div>
       <main className="flex flex-col justify-center items-center w-full bg-gray-900 text-white">
